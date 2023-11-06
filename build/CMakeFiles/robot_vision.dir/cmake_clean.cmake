9a933cc37a7fda9a546c720258bbe30a8cb4f3da
damurka5 <damurka777@gmail.com>
2023-11-06 11:22:08 +0300

    moved to vs code from clion

---
+++ build/CMakeFiles/robot_vision.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_vision.dir/main.cpp.o"
+  "CMakeFiles/robot_vision.dir/main.cpp.o.d"
+  "CMakeFiles/robot_vision.dir/utils/StereoImageProcessing.cpp.o"
+  "CMakeFiles/robot_vision.dir/utils/StereoImageProcessing.cpp.o.d"
+  "robot_vision"
+  "robot_vision.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_vision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
